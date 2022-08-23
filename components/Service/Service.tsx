0e45aca5a099b93fa45e45b9a7fa29d68eb8be62
bhanky <samuel.bankole@quabbly.com>
2022-08-23 02:42:06 +0100

    fix bg color

--- components/Service/Service.tsx
+++ components/Service/Service.tsx
@@ -6,19 +6,17 @@ import Image from 'next/image'
 const service = () => {
   return (
     <Container id={'services'} className={styles.mainService}>
-      <div>
-        <Row className="justify-content-between">
-          {serviceData.map((service) => (
-            <Col md={3} key={service.id} className={styles.service}>
-              <div className={styles.icon}>
-                <Image src={service.img} width={50} height={50} alt="icon" />
-              </div>
-              <p className="fw-semibold">{service.title}</p>
-              <p className="text-dirty-white">{service.text}</p>
-            </Col>
-          ))}
-        </Row>
-      </div>
+      <Row className="justify-content-between">
+        {serviceData.map((service) => (
+          <Col md={3} key={service.id} className={styles.service}>
+            <div className={styles.icon}>
+              <Image src={service.img} width={50} height={50} alt="icon" />
+            </div>
+            <p className="fw-semibold">{service.title}</p>
+            <p className="text-dirty-white">{service.text}</p>
+          </Col>
+        ))}
+      </Row>
     </Container>
   )
 }
